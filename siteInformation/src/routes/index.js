3142cf058e8130a3515a7c0ee508f74a7722b244
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-10-08 11:11:15 +0700

    refactor path import

--- siteInformation/src/routes/index.js
+++ siteInformation/src/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import * as siteInfoControllers from "../controllers/siteInformation/index.js";
+import * as siteInfoControllers from "../controllers/index.js";
 
 const router = express.Router();
 
