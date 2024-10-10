2f9d9f4216bd295637fb936a89a1b229c59d86ed
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-10-10 16:09:17 +0700

    feat: add a new routes for sla3

--- sla/src/routes/index.js
+++ sla/src/routes/index.js
@@ -4,6 +4,6 @@ import * as SlaControllers from "../controllers/index.js";
 const router = express.Router();
 
 router.get("/", SlaControllers.sla);
-
+router.get("/sla3", SlaControllers.sla3Export);
 
 export default router;
