544cdbb8d3ccfca37ce70e5ed99e2699a24674c9
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-10-11 15:49:16 +0700

    api docs: change description

--- sla/swagger/index.js
+++ sla/swagger/index.js
@@ -8,7 +8,7 @@ const swaggerOptions = {
     info: {
       title: "SLA API Documentation",
       version: "1.0.0",
-      description: "SLA API Information",
+      description: "API Documentation for SLA",
     },
     components: {
       schemas: {
